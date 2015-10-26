dd043c08b670913f0d92688e57031a0d6363d375
lilyef2000 <lilyef2000@gmail.com>
2015-10-26 16:22:11 +0800

    modified items.py

--- douban_new_movie/items.py
+++ douban_new_movie/items.py
@@ -11,4 +11,7 @@ import scrapy
 class DoubanNewMovieItem(scrapy.Item):
     # define the fields for your item here like:
     # name = scrapy.Field()
-    pass
+    # pass
+    movie_name=scrapy.Field()
+    movie_star=scrapy.Field()
+    movie_url=scrapy.Field()
